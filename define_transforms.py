85299a1334f32343a547f78d59577e7ffeaa21e3
Fernando Camaro <fernancdo@ascent.ai>
2022-02-08 13:24:42 +0900

    factor out the transformations

---
+++ define_transforms.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+T_zivid_to_world = np.eye(4)
+T_zivid_to_world[0:3,0:3] = np.array([[0.0111658,  0.9582125, -0.2858393],
+                                        [0.9985349,  0.0044512,  0.0539278],
+                                        [0.0529466, -0.2860227, -0.9567589]])
+T_zivid_to_world[0:3,3] = np.array([0.6586, -0.0573, 0.7017])
+np.savetxt('transforms/T_zivid_to_world', T_zivid_to_world)
+
+T_world_to_robot = np.eye(4)
+T_world_to_robot[0,0] = -1
+T_world_to_robot[1,1] = -1
+np.savetxt('transforms/T_world_to_robot', T_world_to_robot)
--- environment.py
+++ environment.py
@@ -14,15 +14,9 @@ class Environment():
         self.robot = Robot(robot_ip)
         self.camera = ZividCamera()
 
-        T_zivid_to_world = np.eye(4)
-        T_zivid_to_world[0:3,0:3] = np.array([[0.0111658,  0.9582125, -0.2858393],
-                                              [0.9985349,  0.0044512,  0.0539278],
-                                              [0.0529466, -0.2860227, -0.9567589]])
-        T_zivid_to_world[0:3,3] = np.array([0.6586, -0.0573, 0.7017])
-        T_world_to_robot = np.eye(4)
-        T_world_to_robot[0,0] = -1
-        T_world_to_robot[1,1] = -1
-        self.T_zivid_robot = T_world_to_robot.dot(T_zivid_to_world)
+        Twz = np.loadtxt("transfroms/T_zivid_to_world.npy")
+        Trw = np.loadtxt("transfroms/T_world_to_robot.npy")
+        self.T_zivid_robot = Trw.dot(Twz)
 
 
     def reset(self):
