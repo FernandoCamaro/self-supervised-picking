c44b72107734e0a44e22c78eaa28f176ab4ba9bd
Fernando Camaro <fernancdo@ascent.ai>
2022-02-12 08:17:16 +0900

    use reward and support new image format

--- human_actor.py
+++ human_actor.py
@@ -15,16 +15,16 @@ def click_event(event, x, y, flags, params):
 env = Environment()
 state = env.reset()
 j,i = 0, 0
-success = None
+reward = None
 # # plt.imshow(state[...,0:3].astype(np.uint8)); plt.show()
 for i in range(10):
-    img = state[...,0:3].astype(np.uint8)
+    img = (state.transpose(1,2,0)*255).astype(np.uint8)
     im_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
     cv2.imshow('image', im_rgb)
     cv2.setMouseCallback('image', click_event)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
-    state, success = env.step([j,i])
-    print("success:", success)
+    state, reward = env.step([j,i])
+    print("rewards:", reward)
     img = state[...,0:3].astype(np.uint8)
